ed5677846bc6e12724202ec352c60323f82c1420
Ray-Phamton <raymonreyesxddd@gmail.com>
2025-08-27 15:40:24 -0400

    update of tic-tac-toe game (5)

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,20 +1,31 @@
+
 import { useState } from "react"
 import confetti from "canvas-confetti"
 import { Square } from "./components/Square"
 import {TURNS} from "./constants.js"
 import {checkWinner,checkEndGame} from "./logic/board.js"
 import {WinnerModal} from "./components/WinnerModal.jsx"
+import { SaveGameStorage,resetGameStorage } from "./logic/Storage.js"
 
 export function App() {
   
-  const [board,setBoard] = useState(Array(9).fill(null))
-  const[turn, setTurn] = useState(TURNS.X)
+  const [board,setBoard] = useState( () => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
+
+  const[turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
   const[winner,setWinner] = useState(null)
   
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage()
   }
 
   const updateBoard = (index)=>{
@@ -27,7 +38,12 @@ export function App() {
     ///change the turn
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
-    
+
+    SaveGameStorage({
+      newBoard: newBoard,
+      newTurn: newTurn
+    })
+
     const newWinner = checkWinner(newBoard)
     if(newWinner){
       confetti()
@@ -69,4 +85,3 @@ export function App() {
     </main>
   )
 }
-
