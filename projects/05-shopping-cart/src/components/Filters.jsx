ef1f594a68dd93fd2eb100b597e6f660af511d42
Ray-Phamton <raymonreyesxddd@gmail.com>
2025-09-17 22:54:05 -0400

    added 'useId' hook from React to create a unique ID

--- projects/05-shopping-cart/src/components/Filters.jsx
+++ projects/05-shopping-cart/src/components/Filters.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useState, useId } from 'react'
 import './Filters.css'
 
 export function Filters ({ onChange }) {
   const [minPrice, setMinPrice] = useState(0)
+  const minPriceFilterId = useId()
+  const categoryFilterId = useId()
 
   const handleChangeMinPrice = (event) => {
     setMinPrice(event.target.value)
@@ -21,10 +23,10 @@ export function Filters ({ onChange }) {
   return (
     <section className='filters'>
       <div>
-        <label htmlFor='price'>Price</label>
+        <label htmlFor={minPriceFilterId}>Price</label>
         <input
           type='range'
-          id='price'
+          id={minPriceFilterId}
           min='0'
           max='1000'
           onChange={handleChangeMinPrice}
@@ -33,8 +35,8 @@ export function Filters ({ onChange }) {
       </div>
 
       <div>
-        <label htmlFor='category'>Categorys</label>
-        <select id='category' onChange={handleChangeCategory}>
+        <label htmlFor={categoryFilterId}>Categorys</label>
+        <select id={categoryFilterId} onChange={handleChangeCategory}>
           <option value='all'>All</option>
           <option value='electronics'>Electronics</option>
           <option value='jewelery'>Jewelery</option>
