4dcf5e7b3718549466d2cadc21f0ab1a4c89fa4a
Ray-Phamton <raymonreyesxddd@gmail.com>
2025-09-18 21:30:56 -0400

    added 'useContext' hook to use Filters

--- projects/05-shopping-cart/src/App.jsx
+++ projects/05-shopping-cart/src/App.jsx
@@ -1,14 +1,12 @@
 import { products as initialProducts } from './mocks/products.json'
 import { Products } from './components/Products'
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
+import { FilterContext } from './context/filters'
 
 function useFilters () {
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0
-  })
+  const { filters, setFilters } = useContext(FilterContext)
 
   const filterProducts = (products) => {
     return products.filter(product => {
@@ -21,6 +19,7 @@ function useFilters () {
       )
     })
   }
+
   return { filters, filterProducts, setFilters }
 }
 
