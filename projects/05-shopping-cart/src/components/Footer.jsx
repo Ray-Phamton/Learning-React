9ab49c55d27391448e94c94146a98c8341ab6d53
Ray-Phamton <raymonreyesxddd@gmail.com>
2025-09-19 18:48:43 -0400

    Added hook 'useReducer' to clean up the code and make it scalable

--- projects/05-shopping-cart/src/components/Footer.jsx
+++ projects/05-shopping-cart/src/components/Footer.jsx
@@ -1,20 +1,12 @@
 import './Footer.css'
-import { useFilters } from '../hooks/useFilters'
+
 export function Footer () {
-  const filters = useFilters()
   return (
     <footer className='footer'>
-      <h4> Active filters:</h4>
-      {
-            JSON.stringify(filters, null, 2)
-        }
-      {/*
       <h4>React test -
         <span>@Ray-Phamton</span>
       </h4>
       <h5>Shooping Cart with useContext & useReducer</h5>
-
-      */}
     </footer>
   )
 }
--- projects/05-shopping-cart/src/components/Products.jsx
+++ projects/05-shopping-cart/src/components/Products.jsx
@@ -1,29 +1,48 @@
+import { useCart } from '../hooks/useCart.js'
+import { AddToCartIcon, RemoveFromCartIcon } from './Icons.jsx'
 import './Products.css'
-import { AddToCartIcon } from './Icons.jsx'
 
 export function Products ({ products }) {
+  const { cart, addToCart, removeFromCart } = useCart()
+  const checkProductInCart = product => {
+    return cart.some(item => item.id === product.id)
+  }
   return (
     <main className='products'>
       <ul>
-        {products.map(product => (
-          <li key={product.id}>
-            <img
-              src={product.thumbnail}
-              alt={product.title}
-            />
-            <div>
-              <strong>{product.title}</strong>
-            </div>
-            <div>
-              <h3>${product.price}</h3>
-            </div>
-            <div>
-              <button>
-                <AddToCartIcon />
-              </button>
-            </div>
-          </li>
-        ))}
+        {products.map(product => {
+          const isProductInCart = checkProductInCart(product)
+          return (
+            <li key={product.id}>
+              <img
+                src={product.thumbnail}
+                alt={product.title}
+              />
+              <div>
+                <strong>{product.title}</strong>
+              </div>
+              <div>
+                <h3>${product.price}</h3>
+              </div>
+              <div>
+                <button
+                  style={{ backgroundColor: isProductInCart ? 'red' : '#09f' }}
+                  onClick={() => {
+                    isProductInCart
+                      ? removeFromCart(product)
+                      : addToCart(product)
+                  }}
+                >
+                  {
+                  isProductInCart
+                    ? <RemoveFromCartIcon />
+                    : <AddToCartIcon />
+                }
+                </button>
+              </div>
+            </li>
+          )
+        })}
       </ul>
     </main>
   )
