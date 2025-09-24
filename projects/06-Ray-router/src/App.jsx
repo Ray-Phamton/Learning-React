86ae504f589e2bcd2be7706b063b5e1ff6c6eaaf
Ray-Phamton <raymonreyesxddd@gmail.com>
2025-09-23 21:46:19 -0400

    the 'Route' component has been created to make it declarative

--- projects/06-Ray-router/src/App.jsx
+++ projects/06-Ray-router/src/App.jsx
@@ -1,4 +1,5 @@
 import { Router } from './components/Router.jsx'
+import { Route } from './components/Route.jsx'
 import HomePage from './pages/Home.jsx'
 import AboutPage from './pages/About.jsx'
 import Page404 from './pages/404.jsx'
@@ -6,14 +7,6 @@ import './App.css'
 import SearchPage from './components/Search.jsx'
 
 const appRoutes = [
-  {
-    path: '/',
-    Component: HomePage
-  },
-  {
-    path: '/about',
-    Component: AboutPage
-  },
   {
     path: '/search/:query',
     Component: SearchPage
@@ -23,7 +16,10 @@ const appRoutes = [
 export function App () {
   return (
     <main>
-      <Router routes={appRoutes} defaultComponent={Page404} />
+      <Router routes={appRoutes} defaultComponent={Page404}>
+        <Route path='/' Component={HomePage} />
+        <Route path='/about' Component={AboutPage} />
+      </Router>
     </main>
   )
 }
--- projects/06-Ray-router/src/components/Router.jsx
+++ projects/06-Ray-router/src/components/Router.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Children } from 'react'
 import { EVENTS } from '../consts'
 import { match } from 'path-to-regexp'
 
-export function Router ({ routes = [], defaultComponent: DefaultComponent = () => <h1>404 </h1> }) {
+export function Router ({ children, routes = [], defaultComponent: DefaultComponent = () => <h1>404 </h1> }) {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
 
   useEffect(() => {
@@ -20,7 +20,16 @@ export function Router ({ routes = [], defaultComponent: DefaultComponent = () =
   }, [])
   let routeParams = {}
 
-  const Page = routes.find(({ path }) => {
+  const routesFromChildren = Children.map(children, ({ props, type }) => {
+    const { name } = type
+    const isRoute = name === 'Route'
+
+    return isRoute ? props : null
+  })
+
+  const routesToUse = routes.concat(routesFromChildren)
+
+  const Page = routesToUse.find(({ path }) => {
     if (path === currentPath) return true
 
     const matcherURL = match(path, { decode: decodeURIComponent })
